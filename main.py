62f410c2661dc8deb61a934220cb1e98e3a2d473
syoto09 <93271277+syoto09@users.noreply.github.com>
2026-01-04 21:19:39 -0600

    updated
    Finish makin the code

--- main.py
+++ main.py
@@ -6,17 +6,37 @@ def sping_row():
 
 def print_row(list):
     print(" ".join(list))
+    bonus_expression(list)
     
 
 def print_result():
     pass
 
+def ten_Times(logo):
+    display = logo * 10
+    return display
+
 
 def bonus_expression(list):
     if list[0] == list[1] == list[2]:
+        display = ""
         if list[0] == '🗾':
-            print("🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾🗾")
-
+            display =  ten_Times( '🗾')
+            print(display)
+        if list[0] == '🍣':
+            display =  ten_Times( '🍣')
+            print(display)
+        if list[0] == '🏯':
+            display =  ten_Times( '🏯')
+            print(display)
+        if list[0] == '🍜':
+            display =  ten_Times( '🍜')
+            print(display)        
+        if list[0] == '🍙':
+            display =  ten_Times( '🍙')
+            print(display)
+        else: 
+            None
 
 def main():
     print("welcome to the Shoto Slots game!")
@@ -24,7 +44,7 @@ def main():
     print("🗾🍣🏯🍜🍙")
 
     while True:
-        contniue = input("if you want to continue: Enter Y & quit: Enter: N:")
+        contniue = input("if you want to continue: Press Enter or quit: Enter N:")
         if contniue == "N":
             break
         
